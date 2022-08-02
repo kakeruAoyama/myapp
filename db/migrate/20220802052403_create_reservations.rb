4197447397e9e071cb7756ec6714db47d223adc8
kakeru Aoyama <kakeru0520sou1@gmail.com>
2022-08-02 14:26:10 +0900

    init

---
+++ db/migrate/20220802052403_create_reservations.rb
@@ -0,0 +1,10 @@
+class CreateReservations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reservations do |t|
+      t.string :title
+      t.date :date
+
+      t.timestamps
+    end
+  end
+end
